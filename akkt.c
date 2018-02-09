ea3cb5839377bf7ef5954cdc71f1a5aa98e1c733
Noah Levitt <nlevitt@archive.org>
2018-02-09 08:26:55 +0000

    improvements

--- akkt.c
+++ akkt.c
@@ -1,9 +1,11 @@
-/*
- *  Disables page protection at a processor level by
- *  changing the 16th bit in the cr0 register (could be Intel specific)
+/* Intercepts execve() syscall and logs info.
  *
- *  Based on example by Peter Jay Salzman and
- *  https://bbs.archlinux.org/viewtopic.php?id=139406
+ * Disables page protection at a processor level by changing the 16th bit in
+ * the cr0 register (could be Intel specific)
+ *
+ * Based on https://github.com/bashrc/LKMPG/blob/d694ae7/4.14.8/examples/syscall.c
+ * which is in turn based on example by Peter Jay Salzman and
+ * https://bbs.archlinux.org/viewtopic.php?id=139406
  */
 
 #include <linux/module.h>
@@ -15,6 +17,7 @@
 #include <linux/unistd.h>       /* The list of system calls */
 #include <linux/sched.h>
 #include <linux/uaccess.h>
+#include <linux/slab.h>
 
 unsigned long **sys_call_table;
 unsigned long original_cr0;
@@ -25,16 +28,26 @@ asmlinkage int wrap_execve (const char *filename,
                             char *const argv[],
                             char *const envp[])
 {
-    int i;
-    pr_info("execve filename='%s'", filename);
-    pr_info("  argv:");
+    int buflen, i;
+    char *buf, *p;
+
+    buflen = 0;
     for (i = 0; argv[i]; i++) {
-        pr_info("    %s", argv[i]);
+        buflen += strlen(argv[i]) + 1;
     }
-    pr_info("  envp:");
-    for (i = 0; envp[i]; i++) {
-        pr_info("    %s", envp[i]);
+
+    p = buf = kmalloc(buflen, GFP_KERNEL);
+    for (i = 0; argv[i]; i++) {
+        strcpy(p, argv[i]);
+        p += strlen(argv[i]) + 1;
+        if (argv[i+1])
+            p[-1] = ' ';
     }
+
+    pr_info("akkt.c: execve uid=%d pid=%d filename=\"%s\" args=\"%s\"\n",
+            __kuid_val(current_uid()), current->pid, filename, buf);
+
+    kfree(buf);
 
     return orig_execve(filename, argv, envp);
 }
@@ -56,52 +69,54 @@ static unsigned long **aquire_sys_call_table(void)
     return NULL;
 }
 
-static int __init syscall_start(void)
+static int __init akkt_init(void)
 {
+    pr_info("akkt.c: loading\n");
     if(!(sys_call_table = aquire_sys_call_table()))
         return -1;
 
+    // disable syscall table page protection
     original_cr0 = read_cr0();
-
     write_cr0(original_cr0 & ~0x00010000);
 
-    /* keep track of the original open function */
+    // replace execve with our wrapper
     orig_execve = (void*)sys_call_table[__NR_execve];
-
-    /* use our open function instead */
     sys_call_table[__NR_execve] = (unsigned long *)wrap_execve;
 
+    // re-enable syscall table page protection
     write_cr0(original_cr0);
 
-    pr_info("will start logging calls to execve (maybe?)");
+    pr_info("akkt.c: loaded: now logging calls to execve\n");
 
     return 0;
 }
 
-static void __exit syscall_end(void)
+static void __exit akkt_exit(void)
 {
     if(!sys_call_table) {
+        pr_info("akkt.c: sys_call_table unset?\n");
         return;
     }
+    pr_info("akkt.c: unloading\n");
 
     /*
      * Return the system call back to normal
      */
     if (sys_call_table[__NR_execve] != (unsigned long *)wrap_execve) {
-        pr_alert("Somebody else also played with the ");
-        pr_alert("execve system call\n");
-        pr_alert("The system may be left in ");
-        pr_alert("an unstable state.\n");
+        pr_alert("Somebody else also played with the execve system call");
+        pr_alert("The system may be left in an unstable state.\n");
     }
 
     write_cr0(original_cr0 & ~0x00010000);
     sys_call_table[__NR_execve] = (unsigned long *)orig_execve;
     write_cr0(original_cr0);
 
-    msleep(2000);
+    pr_info("akkt.c: unloaded\n");
+
+    msleep(2000); // XXX why?
 }
 
-module_init(syscall_start);
-module_exit(syscall_end);
+module_init(akkt_init);
+module_exit(akkt_exit);
 
 MODULE_LICENSE("GPL");
